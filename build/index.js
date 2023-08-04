a185353757ab21de8397eb9d972fc2885517e726
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-08-04 17:49:12 +0100

    Basic command processing

---
+++ build/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import chalk from 'chalk';
+let ranCommand = false;
+yargs(process.argv.slice(2)).scriptName('bsv-utils').command('auth', 'Authenticate with your Relysia account', () => { }, function () {
+    ranCommand = true;
+    console.log('Hi');
+}).help().argv;
+if (!ranCommand) {
+    console.log(chalk.red('Hi'));
+}
